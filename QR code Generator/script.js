95742a0a4af9fb0cc74199ded785b44aa1d0e44c
ShyamJP <118465490+ShyamJP@users.noreply.github.com>
2023-04-20 19:54:36 +0530

    Add files via upload

---
+++ QR code Generator/script.js
@@ -0,0 +1,34 @@
+const wrapper = document.querySelector(".wrapper");
+generateBtn = wrapper.querySelector(".form button");
+qrimage = wrapper.querySelector(".Qr img");
+qrInput = wrapper.querySelector(".form input"); 
+
+prompt("Enter your name:");
+// let a = prompt("Enter Password:");
+let b = document.querySelector("body")
+if(true){
+    generateBtn.addEventListener('click',() => {
+    
+        let qrvalue = qrInput.value;
+        console.log(qrvalue);
+        
+        if(!qrvalue)
+        return;  //if input is empty then return from here
+        generateBtn.innerText = "Generate QR code...";
+    
+        qrimage.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrvalue}`;
+        qrimage.addEventListener("load",() =>{
+            wrapper.classList.add("active");
+    
+            generateBtn.innerText = "Generate QR code";
+        })
+       
+    });
+}
+else{
+    b.innerHTML = "Sorry You Cannot visit my Website"
+}
+
+
+//link for QR code
+//https://goqr.me/api/
